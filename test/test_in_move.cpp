e899bbbf229c0eab64e75021bf59b8a707230b7c
Marcin648 <marcin648@gmail.com>
2021-11-05 23:54:53 +0100

    test: extend move test

--- test/test_in_move.cpp
+++ test/test_in_move.cpp
@@ -5,46 +5,87 @@
 #include "helpers.hpp"
 
 int main(int, char**){
-    TEST_NAME("Instruction MOVE");
-
-    M68K::InstructionDecoder decoder = M68K::InstructionDecoder();
-    M68K::INSTRUCTION::Instruction* instruction = nullptr;
-    uint32_t return_data = 0;
-
-    M68K::CPUState state = M68K::CPUState();
+    TEST_NAME("Instruction MOVE / MOVEA");
     
-    // move.w D1, A1
-    TEST_LABEL("move.w A1, D1");
-    state = M68K::CPUState();
-    state.registers.set(M68K::REG_A1, M68K::DataSize::SIZE_LONG, 0xAABBCCDD);
-    instruction = decoder.Decode(0x3209); // move.w D1, A1
-    instruction->execute(state);
-    return_data = state.registers.get(M68K::REG_D1, M68K::DataSize::SIZE_LONG);
-    TEST_TRUE(return_data == 0xCCDD);
+    {
+        TEST_LABEL("direct - move.w A1, D1");
+        auto instruction = M68K::INSTRUCTION::Move::create(0x3209); // move.w D1, A1
+        M68K::CPUState state = M68K::CPUState();
+        
+        state.registers.set(M68K::REG_A1, M68K::DataSize::SIZE_LONG, 0xAABBCCDD);
 
-    // move.l A0, D1
-    TEST_LABEL("move.l A0, D1");
-    state = M68K::CPUState();
-    state.registers.set(M68K::REG_D1, M68K::DataSize::SIZE_LONG, 0xAABBCCDD);
-    instruction = decoder.Decode(0x2041); // move.l A0, D1
-    instruction->execute(state);
-    return_data = state.registers.get(M68K::REG_A0, M68K::DataSize::SIZE_LONG);
-    TEST_TRUE(return_data == 0xAABBCCDD);
+        instruction.get()->execute(state);
+        uint32_t return_data = state.registers.get(M68K::REG_D1, M68K::DataSize::SIZE_LONG);
+        TEST_TRUE(return_data == 0xCCDD);
+    }
 
-    // Test status register
-    TEST_LABEL("Status register");
-    state = M68K::CPUState();
-    state.registers.set(M68K::REG_D1, M68K::DataSize::SIZE_LONG, 0xAABBCCDD);
-    instruction = decoder.Decode(0x2041); // move.l A0, D1
-    instruction->execute(state);
+    {
+        TEST_LABEL("direct - movea.l A0, D1");
+        auto instruction = M68K::INSTRUCTION::Move::create(0x2041); // movea.l A0, D1
+        M68K::CPUState state = M68K::CPUState();
+        
+        state.registers.set(M68K::REG_D1, M68K::DataSize::SIZE_LONG, 0xAABBCCDD);
 
-    bool flag_negative = state.registers.get(M68K::SR_FLAG_NEGATIVE);
-    bool flag_zero = state.registers.get(M68K::SR_FLAG_ZERO);
-    bool flag_overflow = state.registers.get(M68K::SR_FLAG_OVERFLOW);
-    bool flag_carry = state.registers.get(M68K::SR_FLAG_CARRY);
+        instruction.get()->execute(state);
+        uint32_t return_data = state.registers.get(M68K::REG_A0, M68K::DataSize::SIZE_LONG);
+        TEST_TRUE(return_data == 0xAABBCCDD);
+    }
+    
+    {
+        TEST_LABEL("indirect - move.l (A0), D1");
+        auto instruction = M68K::INSTRUCTION::Move::create(0x2081); // move.l (A0), D1
+        M68K::CPUState state = M68K::CPUState();
+        
+        state.registers.set(M68K::REG_D1, M68K::DataSize::SIZE_LONG, 0xAABBCCDD);
+        state.registers.set(M68K::REG_A0, M68K::DataSize::SIZE_LONG, 0x1000);
 
-    TEST_TRUE(flag_negative == true);
-    TEST_TRUE(flag_zero == false);
-    TEST_TRUE(flag_overflow == 0);
-    TEST_TRUE(flag_carry == 0);
+        instruction.get()->execute(state);
+        uint32_t return_data = state.memory.get(0x1000, M68K::DataSize::SIZE_LONG);
+        TEST_TRUE(return_data == 0xAABBCCDD);
+    }
+
+    {
+        TEST_LABEL("indirect++ - move.l (A0)+, D1");
+        auto instruction = M68K::INSTRUCTION::Move::create(0x20c1); // move.l (A0)+, D1
+        M68K::CPUState state = M68K::CPUState();
+        
+        state.registers.set(M68K::REG_D1, M68K::DataSize::SIZE_LONG, 0xAABBCCDD);
+        state.registers.set(M68K::REG_A0, M68K::DataSize::SIZE_LONG, 0x1000);
+
+        instruction.get()->execute(state);
+        uint32_t return_data = state.registers.get(M68K::REG_A0, M68K::DataSize::SIZE_LONG);
+        TEST_TRUE(return_data == 0x1004);
+    }
+
+    {
+        TEST_LABEL("--indirect - move.l -(A0), D1");
+        auto instruction = M68K::INSTRUCTION::Move::create(0x2101); // move.l -(A0), D1
+        M68K::CPUState state = M68K::CPUState();
+        
+        state.registers.set(M68K::REG_D1, M68K::DataSize::SIZE_LONG, 0xAABBCCDD);
+        state.registers.set(M68K::REG_A0, M68K::DataSize::SIZE_LONG, 0x1000);
+
+        instruction.get()->execute(state);
+        uint32_t return_data = state.registers.get(M68K::REG_A0, M68K::DataSize::SIZE_LONG);
+        TEST_TRUE(return_data == 0x0ffc);
+    }
+
+    {
+        TEST_LABEL("Status register");
+        auto instruction = M68K::INSTRUCTION::Move::create(0x3209); // move.w D1, A1
+        M68K::CPUState state = M68K::CPUState();
+        
+        state.registers.set(M68K::REG_A1, M68K::DataSize::SIZE_LONG, 0xAABBCCDD);
+
+        instruction.get()->execute(state);
+        bool flag_negative = state.registers.get(M68K::SR_FLAG_NEGATIVE);
+        bool flag_zero = state.registers.get(M68K::SR_FLAG_ZERO);
+        bool flag_overflow = state.registers.get(M68K::SR_FLAG_OVERFLOW);
+        bool flag_carry = state.registers.get(M68K::SR_FLAG_CARRY);
+
+        TEST_TRUE(flag_negative == true);
+        TEST_TRUE(flag_zero == false);
+        TEST_TRUE(flag_overflow == 0);
+        TEST_TRUE(flag_carry == 0);
+    }
 }
