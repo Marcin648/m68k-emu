46a59fb0f7aaba044e6d404148b1202f271b9456
Marcin648 <marcin648@gmail.com>
2021-11-10 14:41:07 +0100

    refactor: clean main

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,6 @@
 
 
 int main(int, char**){
-    M68K::CPU cpu;
-    int a = 128;
-    int b = M68K::LSL(a, 1);
-    int c = M68K::ROL_8(a, 1);
-    std::cout << a << " " << b << " " << c << std::endl;
-
-    M68K::Memory memory;
-
-    memory.set(0, M68K::DataSize::SIZE_LONG, 0xAABBCCDD);
-
-    uint32_t data1 = memory.get(0, M68K::DataSize::SIZE_LONG);
-    uint32_t data2 = memory.get(0, M68K::DataSize::SIZE_WORD);
-    uint32_t data3 = memory.get(0, M68K::DataSize::SIZE_BYTE);
-
+    M68K::CPU cpu = M68K::CPU();
     return 0;
 }
