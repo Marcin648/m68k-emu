3232c6cbe5cb66e5c6365cbd8244ebf59cbe59ea
Marcin648 <marcin648@gmail.com>
2021-11-13 17:51:37 +0100

    feat: instruction moveq

--- include/instructions.hpp
+++ include/instructions.hpp
@@ -2,6 +2,7 @@
 #include "instructions/instruction.hpp"
 #include "instructions/illegal.hpp"
 #include "instructions/move.hpp"
+#include "instructions/moveq.hpp"
 #include "instructions/add.hpp"
 #include "instructions/adda.hpp"
 #include "instructions/addx.hpp"
---
+++ src/instructions/moveq.cpp
@@ -0,0 +1,33 @@
+#include "instructions/moveq.hpp"
+#include "instruction_functions.hpp"
+#include "helpers.hpp"
+#include <stdexcept>
+
+using namespace M68K;
+using namespace INSTRUCTION;
+
+Moveq::Moveq(uint16_t opcode) : Instruction(opcode){
+    uint16_t reg_part = (opcode >> 9) & 0x7;
+
+    this->dest_mode = ADDR_MODE_DIRECT_DATA;
+    this->dest_reg = getRegisterType(0, reg_part);
+    this->imm_data = (opcode >> 0) & 0xFF;
+    this->data_size = SIZE_LONG;
+}
+
+void Moveq::execute(CPUState& cpu_state){
+    uint32_t pc = cpu_state.registers.get(REG_PC, SIZE_LONG);
+    pc += 2;
+    cpu_state.registers.set(REG_PC, SIZE_LONG, pc);
+
+    setData(this->dest_mode, this->dest_reg, this->data_size, cpu_state, this->imm_data);
+
+    cpu_state.registers.set(SR_FLAG_NEGATIVE, IS_NEGATIVE(this->imm_data, this->data_size));
+    cpu_state.registers.set(SR_FLAG_ZERO, this->imm_data == 0);
+    cpu_state.registers.set(SR_FLAG_OVERFLOW, false);
+    cpu_state.registers.set(SR_FLAG_CARRY, false);
+}
+
+std::shared_ptr<INSTRUCTION::Instruction> Moveq::create(uint16_t opcode){
+    return std::make_shared<Moveq>(opcode);
+}
