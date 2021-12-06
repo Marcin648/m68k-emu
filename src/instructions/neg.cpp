1a95414a303a1afd81129d40a7c2d5ac5ce60a78
Marcin648 <marcin648@gmail.com>
2021-12-06 20:52:12 +0100

    feat: disassembly for neg

--- src/instructions/neg.cpp
+++ src/instructions/neg.cpp
@@ -55,6 +55,16 @@ void Neg::execute(CPUState& cpu_state){
     cpu_state.registers.set(SR_FLAG_CARRY, IS_CARRY(result, this->data_size));
 }
 
+std::string Neg::disassembly(CPUState& cpu_state){
+    uint32_t pc = cpu_state.registers.get(REG_PC, SIZE_LONG);
+    pc += SIZE_WORD;
+    cpu_state.registers.set(REG_PC, SIZE_LONG, pc);
+
+    std::ostringstream output;
+    output << "neg " << DISASSEMBLER::effectiveAddress(this->dest_mode, this->dest_reg, this->data_size, cpu_state);
+    return output.str();
+}
+
 std::shared_ptr<INSTRUCTION::Instruction> Neg::create(uint16_t opcode){
     return std::make_shared<Neg>(opcode);
 }
