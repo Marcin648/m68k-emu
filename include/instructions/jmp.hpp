b3e22549d541f25701ff8970695faf8135d8237c
Marcin648 <marcin648@gmail.com>
2021-12-06 20:31:35 +0100

    feat: disassembly for jmp, jsr

--- include/instructions/jmp.hpp
+++ include/instructions/jmp.hpp
@@ -13,6 +13,7 @@ namespace M68K{
         public:
             Jmp(uint16_t opcode);
             void execute(CPUState& cpu_state) override;
+            std::string disassembly(CPUState& cpu_state) override;
 
             static std::shared_ptr<INSTRUCTION::Instruction> create(uint16_t opcode);
         };
--- src/instructions/jsr.cpp
+++ src/instructions/jsr.cpp
@@ -30,6 +30,16 @@ void Jsr::execute(CPUState& cpu_state){
     cpu_state.registers.set(REG_PC, SIZE_LONG, dest_addr);
 }
 
+std::string Jsr::disassembly(CPUState& cpu_state){
+    uint32_t pc = cpu_state.registers.get(REG_PC, SIZE_LONG);
+    pc += SIZE_WORD;
+    cpu_state.registers.set(REG_PC, SIZE_LONG, pc);
+
+    std::ostringstream output;
+    output << "jsr " << DISASSEMBLER::effectiveAddress(this->dest_mode, this->dest_reg, this->data_size, cpu_state);
+    return output.str();
+}
+
 std::shared_ptr<INSTRUCTION::Instruction> Jsr::create(uint16_t opcode){
     return std::make_shared<Jsr>(opcode);
 }
