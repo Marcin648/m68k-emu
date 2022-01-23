1f8d5ead292921f51c436f8f94460bdac8cb0360
Marcin648 <marcin648@gmail.com>
2022-01-23 20:16:09 +0100

    test: fix benchmark

--- test/src/benchmark/src/benchmark.c
+++ test/src/benchmark/src/benchmark.c
@@ -2,7 +2,7 @@
 #include "data.h"
 
 int main(void){
-    for(int i = 0; i < 1; i++){
+    for(int i = 0; i < 10; i++){
         if((i & 1) == 0){
             bubblesort(data, 1000);
         }else{
