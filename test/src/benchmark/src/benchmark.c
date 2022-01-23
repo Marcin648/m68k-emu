a0ac7ca03f2d662240589d3d270a777bd8642207
Marcin648 <marcin648@gmail.com>
2022-01-23 19:52:23 +0100

    test: fix benchmark src

--- test/src/benchmark/src/benchmark.c
+++ test/src/benchmark/src/benchmark.c
@@ -3,7 +3,7 @@
 
 int main(void){
     for(int i = 0; i < 10000; i++){
-        if(i & 1 == 0){
+        if((i & 1) == 0){
             bubblesort(data, 30);
         }else{
             bubblesort_reverse(data, 30);
