862922ccb2935b1959ebefd811c117dc716435f1
Marcin648 <marcin648@gmail.com>
2022-01-24 18:53:26 +0100

    test: convert tab to spaces

--- test/src/benchmark/src/bubblesort.c
+++ test/src/benchmark/src/bubblesort.c
@@ -1,25 +1,25 @@
 #include "bubblesort.h"
 
 void bubblesort(int table[], int size){
-	for(int i = 0; i<size-1; i++){
-		for(int j = 0; j<size-1-i; j++){
-			if (table[j] > table[j+1]){
-				int temp = table[j+1];
-				table[j+1] = table[j];
-				table[j] = temp;
-			}
-		}
+    for(int i = 0; i<size-1; i++){
+        for(int j = 0; j<size-1-i; j++){
+            if (table[j] > table[j+1]){
+                int temp = table[j+1];
+                table[j+1] = table[j];
+                table[j] = temp;
+            }
+        }
     }
 }
 
 void bubblesort_reverse(int table[], int size){
-	for(int i = 0; i<size-1; i++){
-		for(int j = 0; j<size-1-i; j++){
-			if (table[j] < table[j+1]){
-				int temp = table[j+1];
-				table[j+1] = table[j];
-				table[j] = temp;
-			}
-		}
+    for(int i = 0; i<size-1; i++){
+        for(int j = 0; j<size-1-i; j++){
+            if (table[j] < table[j+1]){
+                int temp = table[j+1];
+                table[j+1] = table[j];
+                table[j] = temp;
+            }
+        }
     }
 }
--- test/src/bubblesort/src/bubblesort.c
+++ test/src/bubblesort/src/bubblesort.c
@@ -32,14 +32,14 @@ int data[] = {
 };
 
 void bubblesort(int table[], int size){
-	for(int i = 0; i<size-1; i++){
-		for(int j = 0; j<size-1-i; j++){
-			if (table[j] > table[j+1]){
-				int temp = table[j+1];
-				table[j+1] = table[j];
-				table[j] = temp;
-			}
-		}
+    for(int i = 0; i<size-1; i++){
+        for(int j = 0; j<size-1-i; j++){
+            if (table[j] > table[j+1]){
+                int temp = table[j+1];
+                table[j+1] = table[j];
+                table[j] = temp;
+            }
+        }
     }
 }
 
